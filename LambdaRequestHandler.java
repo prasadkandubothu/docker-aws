86201bd642f63d0a8ddc010f86e20a82d1a2a70c
prasadkandubothu <49805047+prasadkandubothu@users.noreply.github.com>
2023-01-27 16:07:05 +0530

    Update LambdaRequestHandler.java

--- LambdaRequestHandler.java
+++ LambdaRequestHandler.java
@@ -1,30 +1,43 @@
 package com.prasad;
 
-
 import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.util.Collections;
 import java.util.Map;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 
-/**
- * Hello world!
- */
-public class LambdaRequestHandler implements RequestHandler<Map<String,String>, String> {
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        public String handleRequest(Map<String,String> input, Context context){
-                LambdaLogger logger = context.getLogger();
-                String response = new String("200 OK");
-                // log execution details
-                logger.log("ENVIRONMENT VARIABLES: " + gson.toJson(System.getenv()));
-                logger.log("CONTEXT: " + gson.toJson(context));
-                // process event
-                logger.log("EVENT: " + gson.toJson(input));
-                logger.log("EVENT TYPE: " + input.getClass().toString());
-                context.getLogger().log("Input: "+input);
-                return gson.toJson(input);
+public class LambdaRequestHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+
+        public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent apiGatewayProxyRequestEvent, Context context){
+                APIGatewayProxyResponseEvent apiGatewayProxyResponseEvent = new APIGatewayProxyResponseEvent();
+                try {
+                        ObjectMapper objectMapper=new ObjectMapper();
+                        String requestString = apiGatewayProxyRequestEvent.getBody();
+                        System.out.println("Input Data : "+requestString);
+
+                        if(requestString != null){
+                                Map map=objectMapper.readValue(requestString, Map.class);
+                                if(!map.isEmpty()){
+                                        generateResponse(apiGatewayProxyResponseEvent, objectMapper.writeValueAsString(map));
+                                }else{
+                                        System.err.println("Input is empty Json");
+                                }
+                        }else{
+                                System.err.println("Input is null");
+                        }
+                } catch (Exception e) {
+                        e.printStackTrace();
+                }
+                return apiGatewayProxyResponseEvent;
+        }
+
+        private void generateResponse(APIGatewayProxyResponseEvent apiGatewayProxyResponseEvent, String requestMessage) {
+                apiGatewayProxyResponseEvent.setHeaders(Collections.singletonMap("timeStamp", String.valueOf(System.currentTimeMillis())));
+                apiGatewayProxyResponseEvent.setStatusCode(200);
+                apiGatewayProxyResponseEvent.setBody(requestMessage);
         }
 }
 
